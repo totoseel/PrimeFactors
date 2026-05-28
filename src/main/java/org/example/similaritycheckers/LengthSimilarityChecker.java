f40564dc1d097e08507215d9164f62cf6ff8ffb0
totoseel <ojinsup@naver.com>
2026-05-28 16:13:10 +0900

    [REFACTOR] Extract MAX_SCORE constant in LengthSimilarityChecker

--- src/main/java/org/example/similaritycheckers/LengthSimilarityChecker.java
+++ src/main/java/org/example/similaritycheckers/LengthSimilarityChecker.java
@@ -2,7 +2,9 @@ package org.example.similaritycheckers;
 
 public class LengthSimilarityChecker {
 
+    private static final int MAX_SCORE = 60;
+
     public int calculate(String a, String b) {
-        return 60;
+        return MAX_SCORE;
     }
 }
